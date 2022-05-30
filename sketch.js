83e0b2f179e48b8295378ee95cf0323640b90097
caioaguilarlima <104800489+caioaguilarlima@users.noreply.github.com>
2022-05-30 19:26:57 -0300

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -10,66 +10,61 @@ function preload(){
   redImageImg = loadImage("redImage.png")
 }
   
-  function setup(){
-    createCanvas(400,400);
-    
-    garden = createSprite(200,200);
-    garden.addImage(gardenImg);
-
-    rabbit = createSprite(200,400,40,40);
-    rabbit.addImage(rabbitImg);
-    rabbit.scale = 0.09;
-
-  }
-
+function setup(){
+  createCanvas(400,400);
   
-  function draw() {
-    background(0);
-    edges = createEdgeSprites();
-    rabbit.collide(edges);
-    rabbit.x = World.mouseX;
-    drawSprites();
-    criarfolhasverdes();
-    criarfolhaslaranjas();
-    criarMacas();
-    var selecionar_objetos = Math.round(random(1,3)); 
-  }
+  garden = createSprite(200,200);
+  garden.addImage(gardenImg);
 
-  if ((frameCount % 80 == 0)) {
-    var randNumber = Math.round(random(10, 80));
-  selecionar_objetos = createSprite(600, 100, 40, 10);
-  selecionar_objetos.y = randNumber;
-  }
- 
+  rabbit = createSprite(200,400,40,40);
+  rabbit.addImage(rabbitImg);
+  rabbit.scale = 0.09;
 
-  function criarfolhasverdes() {
-    leaf = createSprite(330,50,30,30);
-    leaf.addImage(leafImg);
-    leaf.scale = 0.05;
-    leaf. velocityY = 3;
-  }
-
-  function criarfolhaslaranjas() {
-    
-      orangeLeaf = createSprite(120,50,30,30);
-      orangeLeaf.addImage(orangeLeafImg);
-      orangeLeaf.scale = 0.05;
-      orangeLeaf.velocityY = 3;
-  
-      
-  }
-
-  function criarMacas() {
-  
-      apple = createSprite(250,50,30,30);
-      apple.addImage(appleImg);
-      apple.scale = 0.05;
-      apple.velocityY = 3;
-
-  }
-if (condition) {
-  criarMacas();
-} else {
-  criarfolhasverdes();
-  criarfolhaslaranjas();
 }
+
+  
+function draw() {
+  background(0);
+  edges = createEdgeSprites();
+  rabbit.collide(edges);
+  rabbit.x = World.mouseX;
+  var selecionar_objetos = Math.round(random(1,3)); 
+  if ((frameCount % 80 == 0)) {
+    
+    if (selecionar_objetos == 1) {
+      criarfolhasverdes();
+    } else if (selecionar_objetos == 2) {
+      criarfolhaslaranjas();
+    } else {
+      criarMacas();
+    }
+  }
+  drawSprites();
+}
+
+function criarfolhasverdes() {
+  leaf = createSprite(330,50,30,30);
+  leaf.addImage(leafImg);
+  leaf.scale = 0.05;
+  leaf. velocityY = 3;
+}
+
+function criarfolhaslaranjas() {
+  
+    orangeLeaf = createSprite(120,50,30,30);
+    orangeLeaf.addImage(orangeLeafImg);
+    orangeLeaf.scale = 0.05;
+    orangeLeaf.velocityY = 3;
+
+    
+}
+
+function criarMacas() {
+
+    apple = createSprite(250,50,30,30);
+    apple.addImage(appleImg);
+    apple.scale = 0.05;
+    apple.velocityY = 3;
+
+}
+
